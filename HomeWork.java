e722aea00f925d54c4dd25a1a805073354f90b4a
Juliaseid80 <117772166+Juliaseid80@users.noreply.github.com>
2023-03-20 15:18:44 +0100

    Update HomeWork.java

--- HomeWork.java
+++ HomeWork.java
@@ -1,24 +1,41 @@
 package HW;
 
-public class HomeWork {
+public class Numerical {
     public static void main(String[] args) {
 
         int a = 1144;
-        System.out.println(1*Math.pow(10,3) + 1*Math.pow(10,2) + 4*Math.pow(10,1) + 4*Math.pow(10,0));
+        int a10 = 612;
+
+        System.out.println(1 * Math.pow(8, 3) + 1 * Math.pow(8, 2) + 4 * Math.pow(8, 1) + 4 * Math.pow(8, 0));
+        System.out.println();
+
         int b = 978;
-        System.out.println(9*Math.pow(10,2) + 7*Math.pow(10,1) + 8*Math.pow(10,0));
+        int b10 = 640;
+        System.out.println(9 * Math.pow(8, 2) + 7 * Math.pow(8, 1) + 8 * Math.pow(8, 0));
+        System.out.println();
+
         int c = 12;
-        System.out.println(1*Math.pow(10,1) + 2*Math.pow(10,0));
+        int c10 = 10;
+        System.out.println(1 * Math.pow(8, 1) + 2 * Math.pow(8, 0));
+        System.out.println();
 
-        System.out.println(1144 / 8);
-        System.out.println(1144 % 8);
+       
+        int d = 1144;
+        int d8 = 2170;
+        System.out.println(d8);
+        System.out.println();
 
-        System.out.println(978 / 8);
-        System.out.println(978 % 8);
 
-        System.out.println(12 / 8);
-        System.out.println(12 % 8);
+        int e = 978;
+        int e8 = 1722;
+        System.out.println(e8);
+        System.out.println();
 
+
+        int f = 12;
+        int f8 = 14;;
+        System.out.println(f8);
 
     }
 }
+
